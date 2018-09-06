129d320ce0aa3fcc8e8926fbd000494539c255f3
Ibrahim Olukorede Folarin <ifolarin@webbasedsol.com>
2018-09-06 23:42:45 +0100

    renamed proxy network to proxy-net

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ~
+
+mkdir /srv
+
+chown -R :docker /srv
+chmod g+rwx :docker /srv
+
+git clone https://github.com/ifolarin/docker-wordpress.git frontend
+
+docker network create --driver bridge proxy-net || true
+docker network create --internal --driver bridge db-net || true
+
+cp frontend/proxy /srv/proxy
+cd /srv/proxy 
+
+docker-compose up -d
