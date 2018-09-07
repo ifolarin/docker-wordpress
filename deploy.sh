0b2a84be364d120c3f50d9743894f635d915ba1e
Ibrahim Olukorede Folarin <ifolarin@webbasedsol.com>
2018-09-07 01:07:18 +0100

    removed redundant git clone directive

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,6 @@ mkdir /srv
 chown -R :docker /srv
 chmod g+rwx :docker /srv
 
-git clone https://github.com/ifolarin/docker-wordpress.git frontend
-
 docker network create --driver bridge proxy-net || true
 docker network create --internal --driver bridge db-net || true
 
